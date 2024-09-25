ff5dab3dac068cf3906f84146bd145e2cc66c03e
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-09-25 11:19:16 +0530

    Swtiching Between Scenes

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+   public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
